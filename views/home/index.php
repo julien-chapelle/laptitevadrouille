353a559f1683d2886b52af6d4a915033f15dd69d
Chapelle Julien <you@example.comLHP3.cjulien@gmail.com>
2020-03-06 11:34:55 +0100

    création et positionnement map accueil ok

--- views/home/index.php
+++ views/home/index.php
@@ -1,11 +1,19 @@
-<div class="row d-flex justify-content-end m-0">
-    <div class="col-7 p-0">
-        <div class="text-center pt-5">
-            <button type="button" class="btn buttonColor2 btn-sm mr-5">
-                <span class="mr-3 h3"><i class="fas fa-crosshairs"></i></span>Rechercher autour de moi</button>
-        </div>
+<div class="row d-flex justify-content-center m-0">
+    <!-- <div class="col-lg-7 col-md-10 col-sm-12 col-12 text-center">
+        <img src="assets/img/seineMaritime.png" title="Image carte Seime-Maritime" alt="Image carte Seime-Maritime" class="mapHomePage img-fluid moveEffectMap" />
+        <img src="assets/img/eure.png" title="Image carte Eure" alt="Image carte Eure" class="mapHomePage img-fluid moveEffectMap" />
+        <img src="assets/img/orne.png" title="Image carte Orne" alt="Image carte Orne" class="mapHomePage img-fluid moveEffectMap" />
+        <img src="assets/img/manche.png" title="Image carte Manche" alt="Image carte Manche" class="mapHomePage img-fluid moveEffectMap" />
+        <img src="assets/img/calvados.png" title="Image carte Calvados" alt="Image carte Calvados" class="mapHomePage img-fluid moveEffectMap" />
+    </div> -->
+    <div class="col-lg-7 col-md-10 col-sm-12 col-12 text-center mt-5">
+        <img src="assets/img/seine_maritime2.png" title="Image carte Seime-Maritime" alt="Image carte Seime-Maritime" class="mapHomePageSeineMaritime img-fluid moveEffectMap" />
+        <img src="assets/img/eure2.png" title="Image carte Eure" alt="Image carte Eure" class="mapHomePageEure img-fluid moveEffectMap" />
+        <img src="assets/img/orne2.png" title="Image carte Orne" alt="Image carte Orne" class="mapHomePageOrne img-fluid moveEffectMap" />
+        <img src="assets/img/manche2.png" title="Image carte Manche" alt="Image carte Manche" class="mapHomePageManche img-fluid moveEffectMap" />
+        <img src="assets/img/calvados2.png" title="Image carte Calvados" alt="Image carte Calvados" class="mapHomePageCalvados img-fluid moveEffectMap" />
     </div>
-    <div class="col-5 p-0 text-right textColor2">
+    <div class="col-lg-2 col-md-8 col-sm-12 col-12 p-0 text-center textColor2">
         <ul class="my-5">
             <li>Le Havre</li>
             <li>Rouen</li>
@@ -21,5 +29,13 @@
             <li>Argentan</li>
             <li>Gisors</li>
         </ul>
+    </div>
+</div>
+<div class="row d-flex justify-content-start m-0">
+    <div class="col-7 p-0">
+        <div class="text-center pt-5">
+            <button type="button" class="btn buttonColor2 btn-sm mr-5">
+                <span class="mr-3 h3"><i class="fas fa-crosshairs"></i></span>Rechercher autour de moi</button>
+        </div>
     </div>
 </div>
