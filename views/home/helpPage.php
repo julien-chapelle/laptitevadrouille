17d2ea43ee6890ca96bd943e3d6b727bcf125115
Chapelle Julien <you@example.comLHP3.cjulien@gmail.com>
2020-03-13 13:59:23 +0100

    aide terminé

--- views/home/helpPage.php
+++ views/home/helpPage.php
@@ -18,7 +18,7 @@
             <div class="card card-body textColor2">
                 <div class="row m-0">
                     <div class="col-12 p-0">
-                        <p class="textColor2"><span class="textColor1 h4">1 -</span> Pour vous inscrire il vous suffit de vous rendre sur la page d'Inscription : <a href="http://laptitevadrouille/index.php?user=add" title="Aller vers inscription" class="textColor1">CLIQUEZ ICI</a></p>
+                        <p class="textColor2"><span class="textColor1 h4">1 -</span> Pour vous inscrire il vous suffit de vous rendre sur la page d'Inscription : <a href="http://laptitevadrouille/index.php?user=add" title="Aller vers inscription" class="textColor1 borderHepl rounded p-1">CLIQUEZ ICI</a></p>
                     </div>
                     <div class="col-12 p-0">
                         <p class="textColor2"><span class="textColor1 h4">2 -</span> Vous devrez ensuite renseigner :</p>
@@ -26,7 +26,7 @@
                             <li class="textColor2">Un nom ou pseudonyme ne contenant aucun symboles ou caractères particuliers (uniquement lettres Maj/Min, accentuées, espace, tiret et chiffres)</li>
                             <li class="textColor2">Une adresse mail valide au format mail en vigueur</li>
                             <li class="textColor2">Un mot de passe de moins de 15 caractères (uniquement lettres Maj/Min et chiffres)</li>
-                            <li class="textColor2">Cliquer sur le bouton "S'INSCRIRE"</li>
+                            <li class="textColor2">Cliquer sur le bouton <span class="textColor1 borderHepl rounded p-1">"S'INSCRIRE"</span></li>
                         </ul>
                     </div>
                     <div class="col-12 p-0">
@@ -54,30 +54,30 @@
                     <div class="col-12 p-0">
                         <p class="textColor2"><span class="textColor1 h4">1 -</span> Pour voir vôtre espace utilisateur, vous devez vous connecter à l'aide de vôtre adresse mail et vôtre mot de passe :</p>
                         <ul>
-                            <li class="textColor2">Pour cela cliquez sur l'icone en haut à droite dans la barre de navigation : <span class="h3"><i class="far fa-user-circle textColor2"></i></span></li>
-                            <li class="textColor2">Renseignez vôtre mail et mot de passe puis cliquez sur le bouton "CONNEXION"</li>
+                            <li class="textColor2">Pour cela cliquez sur l'icone en haut à droite dans la barre de navigation : <span class="textColor1 h3"><i class="far fa-user-circle"></i></span></li>
+                            <li class="textColor2">Renseignez vôtre mail et mot de passe puis cliquez sur le bouton <span class="textColor1 borderHepl rounded p-1">"CONNEXION"</span></li>
                             <li class="textColor2">Si la connexion a réussie, vous serez automatiquement redirigé vers vôtre page d'informations</li>
                         </ul>
                     </div>
                     <div class="col-12 p-0">
                         <p class="textColor2"><span class="textColor1 h4">2 -</span> Modification des données :</p>
                         <ul>
-                            <li class="textColor2">- Vous pouvez modifier vôtre mail ou vôtre nom/pseudonyme depuis vôtre espace en cliquant sur l'icone : <span class="h3"><i class="far fa-edit"></i></span></li>
+                            <li class="textColor2">- Vous pouvez modifier vôtre mail ou vôtre nom/pseudonyme depuis vôtre espace en cliquant sur l'icone : <span class="textColor1 h3"><i class="far fa-edit"></i></span></li>
                             <ul>
                                 <li class="textColor2">Saisir vôtre nouveau nom/pseudonyme ou mail</li>
                                 <li class="textColor2">Saisir vôtre mot de passe</li>
-                                <li class="textColor2">Cliquez sur le bouton "MODIFIER"</li>
+                                <li class="textColor2">Cliquez sur le bouton <span class="textColor1 borderHepl rounded p-1">"MODIFIER"</span></li>
                                 <li class="textColor2">En cas de succès vous reviendrez automatiquement sur vôtre espace utilisateur</li>
                             </ul>
-                            <li class="textColor2">- Vous pouvez également modifier vôtre mot de passe en cliquant sur le bouton "MODIFIER MON MOT DE PASSE"</li>
+                            <li class="textColor2">- Vous pouvez également modifier vôtre mot de passe en cliquant sur le bouton <span class="textColor1 borderHepl rounded p-1">"MODIFIER MON MOT DE PASSE"</span></li>
                             <ul>
                                 <li class="textColor2">Saisir vôtre mot de passe actuel</li>
                                 <li class="textColor2">Saisir vôtre nouveau mot de passe</li>
                                 <li class="textColor2">Confirmer vôtre nouveau mot de passe</li>
-                                <li class="textColor2">Cliquez sur le bouton "MODIFIER"</li>
+                                <li class="textColor2">Cliquez sur le bouton <span class="textColor1 borderHepl rounded p-1">"MODIFIER"</span></li>
                                 <li class="textColor2">En cas de succès vous reviendrez automatiquement sur vôtre espace utilisateur</li>
                             </ul>
-                            <li class="textColor2">- Vous pouvez aussi choisir un avatar depuis une liste de proposition en cliquant sur le bouton "CHOISIR AVATAR"</li>
+                            <li class="textColor2">- Vous pouvez aussi choisir un avatar depuis une liste de proposition en cliquant sur le bouton <span class="textColor1 borderHepl rounded p-1">"CHOISIR AVATAR"</span></li>
                             <ul>
                                 <li class="textColor2">Cliquez sur un avatar pour choisir</li>
                                 <li class="textColor2">Vous reviendrez automatiquement sur vôtre espace utilisateur</li>
@@ -87,10 +87,10 @@
                     <div class="col-12 p-0">
                         <p class="textColor2"><span class="textColor1 h4">3 -</span> Suppression du compte utilisateur :</p>
                         <ul>
-                            <li class="textColor2">- Vous pouvez supprimer vôtre compte depuis vôtre espace en cliquant sur le bouton : "SUPPRIMER LE COMPTE"</li>
+                            <li class="textColor2">- Vous pouvez supprimer vôtre compte depuis vôtre espace en cliquant sur le bouton : <span class="textColor1 borderHepl rounded p-1">"SUPPRIMER LE COMPTE"</span></li>
                             <ul>
                                 <li class="textColor2">Saisir vôtre mot de passe</li>
-                                <li class="textColor2">Cliquez sur le bouton "SUPPRIMER"</li>
+                                <li class="textColor2">Cliquez sur le bouton <span class="textColor1 borderHepl rounded p-1">"SUPPRIMER"</span></li>
                                 <li class="textColor2">En cas de succès vous reviendrez automatiquement sur la page d'accueil du site</li>
                                 <li class="text-danger"><i class="fas fa-exclamation-triangle"></i> Vos informations et vôtre compte seront définitivement supprimés !</li>
                             </ul>
@@ -110,7 +110,40 @@
         </h2>
         <div class="collapse text-left" id="collapsehelp3">
             <div class="card card-body textColor2">
-                Pour déposer une annonce il vous suffit de...
+                <div class="row m-0">
+                    <div class="col-12 p-0">
+                        <p class="textColor2"><span class="textColor1 h4">1 -</span> Pour proposer une sortie, vous devez être connecté à vôtre compte :</p>
+                        <ul>
+                            <li class="textColor2">Pour les informations relatives à la connexion au compte utilisateur, se référer à la section "Gérer le compte" de la page d'aide</li>
+                        </ul>
+                    </div>
+                    <div class="col-12 p-0">
+                        <p class="textColor2"><span class="textColor1 h4">2 -</span> Une fois connecté, vous devez vous rendre dans vôtre espace utilisateur et cliquer sur le bouton <span class="textColor1 borderHepl rounded p-1">"PROPOSER UNE SORTIE"</span> et remplir le formulaire comme suit :</p>
+                        <ul>
+                            <li class="textColor2 my-3"><span class="textColor1 borderHepl rounded p-1">- Titre -</span> Saisir le nom officiel de la sortie sans symboles ou caractères particuliers (uniquement lettres Maj/Min, accentuées, espace, tiret et chiffres)</li>
+                            <li class="textColor2 my-3"><span class="textColor1 borderHepl rounded p-1">- Description courte -</span> Saisir une description courte et explicite qui sera visible sur les mini-fiches de la liste des sorties</li>
+                            <li class="textColor2 my-3"><span class="textColor1 borderHepl rounded p-1">- Description complète -</span> Saisir une description détaillée qui sera visible sur la fiche de détail de la sortie concernée</li>
+                            <li class="textColor2 my-3"><span class="textColor1 borderHepl rounded p-1">- Prix âge 0-3 ans -</span> Saisir le prix pour cette tranche d'âge qui peu être un nombre ou le mot "GRATUIT"</li>
+                            <li class="textColor2 my-3"><span class="textColor1 borderHepl rounded p-1">- Prix âge 3-11 ans -</span> Saisir le prix pour cette tranche d'âge qui peu être un nombre ou le mot "GRATUIT"</li>
+                            <li class="textColor2 my-3"><span class="textColor1 borderHepl rounded p-1">- Prix âge 12 ans et plus -</span> Saisir le prix pour cette tranche d'âge qui peu être un nombre ou le mot "GRATUIT"</li>
+                            <li class="textColor2 my-3"><span class="textColor1 borderHepl rounded p-1">- Prix enfant en situation de handicape -</span> Saisir le prix pour cette tranche d'âge qui peu être un nombre ou le mot "GRATUIT" ou "INCONNU" si non renseigné</li>
+                            <li class="textColor2 my-3"><span class="textColor1 borderHepl rounded p-1">- Heures et périodes d'ouvertures -</span> Saisir suivant un format clair (ex. Mois à Mois : de 00h00 à 00h00) les périodes d'ouvertures avec leurs horaires respectives ainsi que les périodes de fermetures annuelles si renseignés</li>
+                            <li class="textColor2 my-3"><span class="textColor1 borderHepl rounded p-1">- Site officiel -</span> Saisir uniquement l'adresse (Url) du site officiel de la sortie (ne sont pas autorisé les adresses Facebook, Twitter ou Instagram). Si la sortie ne possède qu'un des sites précédents, contacter l'administrateur</li>
+                            <li class="textColor2 my-3"><span class="textColor1 borderHepl rounded p-1">- Pictogram localisation -</span> Choisir entre sortie en extérieur ou intérieur</li>
+                            <li class="textColor2 my-3"><span class="textColor1 borderHepl rounded p-1">- Pictogram type de sortie -</span> Choisir un type de sortie parmi la liste</li>
+                            <li class="textColor2 my-3"><span class="textColor1 borderHepl rounded p-1">- Pictogram âge minimal conseillé -</span> Choisir entre 0, 3 et 5ans. Ce choix est propre à vôtre ressenti et expérience</li>
+                            <li class="textColor2 my-3"><span class="textColor1 borderHepl rounded p-1">- Pictogram transport conseillé -</span> Choisir entre poussette ou porte bébé. Ce choix est propre à vôtre ressenti et expérience</li>
+                            <li class="textColor2 my-3"><span class="textColor1 borderHepl rounded p-1">- Pictogram plan à langer -</span> Cocher si l'équipement est disponible dans la sortie</li>
+                            <li class="textColor2 my-3"><span class="textColor1 borderHepl rounded p-1">- Pictogram paiements acceptés -</span> Cocher les modes de paiements acceptés, si tout est gratuit, cocher la case correspodante</li>
+                            <li class="textColor2 my-3">Cliquez sur le bouton <span class="textColor1 borderHepl rounded p-1">"CREER"</span></li>
+                            <li class="textColor2 my-3">En cas de succès vous reviendrez automatiquement sur vôtre espace utilisateur</li>
+                            <li class="textColor2 my-3">La validation avant mise en ligne, la modification, la suppression et les informations complémentaires seront uniquement à la charge de l'administrateur</li>
+                        </ul>
+                    </div>
+                    <div class="col-12 p-0">
+                        <p class="textColor2"><span class="textColor1 h4">3 -</span> En cas de problème pour ajouter une sortie, contactez l'administrateur via la page contact en cliquant sur le lien <span class="textColor1 borderHepl rounded p-1">"Nous contacter"</span> en bas de la page</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
